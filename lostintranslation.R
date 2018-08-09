4cc39ca9da1e723814a5f9ec01dbf1ec74597d8f
papapalpatine <ben.thevathasan@gmail.com>
2018-08-09 17:40:29 -0500

    Cyclic convergence (?) exit
    Exit main function if trapped in a cycle ie. translating a (long) past re-translation.
    
    Smaller changes - more robust library load

--- lostintranslation.R
+++ lostintranslation.R
@@ -1,14 +1,45 @@
-library(jsonlite)
-library(translateR)
-library(utils)
+#Specify and load libraries
+librariesToLoad <- c('jsonlite', 'translateR', 'utils')
 
+
+sapply(librariesToLoad, function(package) {
+  if(require(package, character.only=TRUE)){     
+    print(paste(package, "loaded correctly", sep=" "))
+  } else {
+    print(paste("Installing", package, sep=" "))
+    install.packages(package)
+    if(require(package, character.only=TRUE)){
+      print(paste(package, "loaded correctly", sep=" "))
+    } else {
+      stop(paste("Could not install", package, sep=" "))
+    }
+  }
+})
+
+
+
+search()
+
+
+
+
+#Specify unique API Key
+#key <- PUTYOURAPIKEYHERE
+
+#Specify main translation paramaters
+#What to translate
+translate_this <- 'Greetings citizen! how are you doing this fine day?'
+#Default source & target languages
+default_source_language <- 'en'
+default_target_language <- 'zh-CN'
+
+
+#Print language codes
 getGoogleLanguages()
 
 
 
-#key <- PUTYOURAPIKEYHERE
-
-
+#Create simple translation function
 google_translate <- function(translate_this, source_lang, target_lang, key) {
   base_url <- 'https://www.googleapis.com/language/translate/v2'
   query <- URLencode(paste0(base_url, '?key=', key, '&q=', translate_this, '&source=', source_lang, '&target=', target_lang))
@@ -16,21 +47,39 @@ google_translate <- function(translate_this, source_lang, target_lang, key) {
   text <- unlist(result)
   return(text)
 }
-translate_this <- 'Greetings citizen! how are you doing this fine day?'
+
+#Test translate
+translate_this
 google_translate(translate_this = translate_this, source_lang = 'en', target_lang = 'zh-CN', key = key)
 google_translate(translate_this = google_translate(translate_this = translate_this, source_lang = 'en', target_lang = 'zh-CN', key = key), 
                  source_lang = 'zh-CN', target_lang = 'en', key = key)
 
-lost_in_translation <- function(original_text, iterations, source_lang, target_lang, key) {
+#Create function for iterative translations
+lost_in_translation <- function(original_text, iterations, source_lang, target_lang, key, print_intermediates = FALSE) {
   translate_this <- original_text
+  all_translations <- rep(NA, iterations)
+  all_retranslations <- rep(NA, iterations)
   for (i in 1:iterations) {
     translation <- google_translate(translate_this = translate_this, source_lang = source_lang, target_lang = target_lang, key = key)
+    all_translations[i] <- translation
     retranslation <- google_translate(translate_this = translation, source_lang = target_lang, target_lang = source_lang, key = key)
+    all_retranslations[i] <- retranslation
+    if(print_intermediates == TRUE) {
+      print(retranslation)
+    }
     #Stopping mechanism w/ convergence
     if(retranslation == translate_this) {
-      print(paste("Convergence after", i, "iterations"))
+      print(paste("Convergence after", i-1, "iterations"))
       return(retranslation)
-    } else {
+    } else  {
+      if(i > 1) {
+        if(retranslation %in% all_retranslations[1:(i-1)]) {
+          #Cyclic convergence (?)
+          print(paste("Trapped in cyclic convergence after", which(retranslation == all_retranslations[1:(i-1)]), "iterations"))
+          return(retranslation)
+        }
+        
+      }
       translate_this <- retranslation
     }
   }
@@ -38,11 +87,22 @@ lost_in_translation <- function(original_text, iterations, source_lang, target_l
   return(final_text)
 }
 
-lost_in_translation(original_text = translate_this, iterations = 5, source_lang = 'en', target_lang = 'zh-CN', key = key)
 
 
 
-lost_in_translation(original_text = 'emotion recollected in tranquility', iterations = 50, source_lang = 'en', target_lang = 'zh-CN', key = key)
+
+#Execute main function
+translate_this
+default_source_language
+default_target_language
+lost_in_translation(original_text = translate_this, iterations = 5, source_lang = default_source_language, target_lang = default_target_language, key = key)
 
 
+#Additional runs
+lost_in_translation(original_text = 'emotion recollected in tranquility', iterations = 100, source_lang = default_source_language, target_lang = default_target_language, key = key, print_intermediates = TRUE)
 
+
+lost_in_translation(original_text = 'need air-conditioning lest leisure take its toll', iterations = 100, source_lang = default_source_language, target_lang = 'ta', key = key)
+
+
+lost_in_translation(original_text = 'angelheaded hipsters burning for the ancient heavenly connection to the starry dynamo in the machinery of night', iterations = 100, source_lang = default_source_language, target_lang = 'ta', key = key, print_intermediates = TRUE)
